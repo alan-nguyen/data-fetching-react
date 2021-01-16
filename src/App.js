2932ab2c7b0ee8a4f9fabc24414d2b900b19f1ba
Alan Nguyen <tuan8101@gmail.com>
2021-01-16 06:15:23 -0700

    Build Search feature

--- src/App.js
+++ src/App.js
@@ -21,8 +21,13 @@ export default class App extends Component {
     //   .catch((error) => {
     //     console.log('Error fetching and parsing data', error);
     //   });
+  }
+
+  performSearch = (query) => {
     axios
-      .get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+      .get(
+        `http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=BG0uinTSDtfym4Q3P2REbOkh7wHDqQz3`
+      )
       .then((response) => {
         this.setState({
           gifs: response.data.data,
@@ -31,7 +36,7 @@ export default class App extends Component {
       .catch((error) => {
         console.log('Error fetching and parsing data', error);
       });
-  }
+  };
 
   render() {
     return (
@@ -39,7 +44,7 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />
+            <SearchForm onSearch={this.performSearch} />
           </div>
         </div>
         <div className="main-content">
