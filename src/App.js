59f100899407628736227bd871e94cf59d685cd3
Alan Nguyen <tuan8101@gmail.com>
2021-01-17 12:01:58 -0700

    Refractor using Hook

--- src/App.js
+++ src/App.js
@@ -1,63 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      gifs: [],
-      loading: true,
-    };
-  }
+function App() {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState('cats');
+  const [isLoading, setIsLoading] = useState(true);
 
-  componentDidMount() {
-    this.performSearch();
-    // fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-    //   .then((response) => response.json())
-    //   .then((responseData) => {
-    //     this.setState({ gifs: responseData.data });
-    //   })
-    //   .catch((error) => {
-    //     console.log('Error fetching and parsing data', error);
-    //   });
-  }
+  // update the query state
+  const performSearch = (value) => setQuery(value);
 
-  performSearch = (query = 'cats') => {
+  useEffect(() => {
     axios
       .get(
         `https://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=BG0uinTSDtfym4Q3P2REbOkh7wHDqQz3`
       )
-      .then((response) => {
-        this.setState({
-          gifs: response.data.data,
-          loading: false,
-        });
-      })
-      .catch((error) => {
-        console.log('Error fetching and parsing data', error);
-      });
-  };
+      .then((response) => setData(response.data.data))
+      .catch((error) => console.log('Error fetching and parsing data', error))
+      .finally(() => setIsLoading(false));
+  }, [query]); // isLoading and data are being updated within the hook (setState), no need to pass them to the dependency array
 
-  render() {
-    return (
-      <div>
-        <div className="main-header">
-          <div className="inner">
-            <h1 className="main-title">GifSearch</h1>
-            <SearchForm onSearch={this.performSearch} />
-          </div>
-        </div>
-        <div className="main-content">
-          {this.state.loading ? (
-            <p>Loading...</p>
-          ) : (
-            <GifList data={this.state.gifs} />
-          )}
+  return (
+    <div>
+      <div className="main-header">
+        <div className="inner">
+          <h1 className="main-title">GifSearch</h1>
+          <SearchForm onSearch={performSearch} />
         </div>
       </div>
-    );
-  }
+      <div className="main-content">
+        {isLoading ? <p>Loading...</p> : <GifList data={data} />}
+      </div>
+    </div>
+  );
 }
+
+export default App;
